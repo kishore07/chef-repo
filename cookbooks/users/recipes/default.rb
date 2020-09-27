5501fd31b04fcd350e3bed277494d75e3798dc83
kishore07 <kishore.senagana@gmail.com>
2020-09-28 00:46:10 +0530

    Adding users cookbook

---
+++ cookbooks/users/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: users
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
---
+++ cookbooks/users/recipes/usercreate.rb
@@ -0,0 +1,11 @@
+## To create users 
+
+#users=['user1','user2','user3']
+
+user 'user1' do
+action :create
+end
+
+
+
+
